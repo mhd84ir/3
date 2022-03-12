1f3d9da38288b8bedf9a7a809c1bd62260d0b289
unknown <mhd84.ir@gmail.com>
2022-03-12 11:01:55 +0330

    3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int money;
+
+System.Console.WriteLine("hoghogh khod ra vared konid");
+
+money = Convert.ToInt32(Console.ReadLine());
+
+
+if (money < 2000000)
+{
+    System.Console.WriteLine("shoma moaf hastid");
+
+}
+
+else if (money > 2000000 && money < 3000000)
+{
+    System.Console.WriteLine(money * 0.05);
+
+}
+
+else
+{
+
+    System.Console.WriteLine(money * 0.07);
+
+}
+
+
